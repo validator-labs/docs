518b8b00cc1ac69e4c5b48f82abc54d396ce4de7
Matt Welke <matt.welke@spectrocloud.com>
2024-08-28 15:09:43 -0400

    Set url property
    Signed-off-by: Matt Welke <matt.welke@spectrocloud.com>

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://validator-labs.github.io/',
+  url: 'https://validator-labs.github.io/docs',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
