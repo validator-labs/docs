6eff6484e41050863762f7550aafc52696d7d416
Tyler Gillson <tyler.gillson@gmail.com>
2025-08-15 08:47:18 -0700

    chore: fix get_validator.sh for darwin/arm64

--- scripts/get_validator.sh
+++ scripts/get_validator.sh
@@ -5,6 +5,7 @@ arch=$(uname -m)
 case "$arch" in
     x86_64) arch="amd64" ;;
     aarch64) arch="arm64" ;;
+    arm64 arch="arm64" ;;
     *)
         echo "Unsupported CPU architecture: $arch"
         exit 1
